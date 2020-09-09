208ee36b8409a19a37151ab0332c8919a23bb00c
ctarrington <arrington.ct@gmail.com>
2020-09-09 19:37:34 -0400

    added second order KF

--- bumperboats/track.py
+++ bumperboats/track.py
@@ -5,9 +5,9 @@ from filterpy.kalman import KalmanFilter
 import filterpy.common
 
 
-class SimplePositionKFTrack:
+class SimpleFirstOrderKFTrack:
     def __init__(self, dt, std):
-        self.kf = kf = KalmanFilter(dim_x=4, dim_z=2)
+        self.kf = KalmanFilter(dim_x=4, dim_z=2)
 
         # state transition function
         self.kf.F = np.array([
@@ -19,7 +19,7 @@ class SimplePositionKFTrack:
 
         # Process noise
         q = filterpy.common.Q_discrete_white_noise(dim=2, dt=dt, var=0.1)
-        kf.Q = block_diag(q, q)
+        self.kf.Q = block_diag(q, q)
 
         # measurement matrix state -> measurement space
         self.kf.H = np.array([
@@ -32,6 +32,50 @@ class SimplePositionKFTrack:
 
         # initial covariance matrix - weak flat prior
         self.kf.P = np.eye(4) * 500.
+
+        # Measurement noise
+        self.kf.R = np.eye(2).dot(std*std)
+
+    def on_data(self, z_measurement):
+        self.kf.predict()
+        self.kf.update(z_measurement)
+
+    def get_position_estimate(self):
+        return self.kf.H.dot(self.kf.x)
+
+    def print_diagnostics(self):
+        print('kf\n', self.kf)
+
+
+class SimpleSecondOrderKFTrack:
+    def __init__(self, dt, std):
+        self.kf = KalmanFilter(dim_x=6, dim_z=2)
+
+        # state transition function
+        self.kf.F = np.array([
+            [1, dt, 0.5*dt*dt, 0, 0, 0],
+            [0,  1, dt,        0, 0, 0],
+            [0,  0, 1,         0, 0, 0],
+            [0,  0, 0,         1, dt, 0.5*dt*dt],
+            [0,  0, 0,         0, 1,  dt],
+            [0,  0, 0,         0,  0, 1],
+        ])
+
+        # Process noise
+        q = filterpy.common.Q_discrete_white_noise(dim=3, dt=dt, var=0.1)
+        self.kf.Q = block_diag(q, q)
+
+        # measurement matrix state -> measurement space
+        self.kf.H = np.array([
+            [1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0]
+        ])
+
+        # initial state
+        self.kf.x = np.array([[0, 0, 0, 0, 0, 0]]).T
+
+        # initial covariance matrix - weak flat prior
+        self.kf.P = np.eye(6) * 500.
 
         # Measurement noise
         self.kf.R = np.eye(2).dot(std*std)
--- samples/simple.py
+++ samples/simple.py
@@ -4,10 +4,11 @@ from bumperboats.boat import Boat
 from bumperboats.controller import FixedController
 from bumperboats.physics import SimpleEngine
 from bumperboats.sensors import SimplePositionSensor
-from bumperboats.track import SimplePositionKFTrack
+from bumperboats.track import SimpleSecondOrderKFTrack
 from bumperboats.viewer import PlotViewer
 
-track = SimplePositionKFTrack(dt=1, std=3)
+track = SimpleSecondOrderKFTrack(dt=1, std=3)
+
 engine = SimpleEngine()
 sensor = SimplePositionSensor(engine, std=3, period=1)
 sensor.add_destination(track)
@@ -16,7 +17,6 @@ viewer = PlotViewer(engine, sensor, track)
 boat = Boat(np.array([325., 325.]), 0., np.array([0., 0.]), np.array([0.5, 0.]))
 controller = FixedController(thrust=1.25, thrust_angle=30.)
 engine.add_boat(boat, controller)
-
 
 dt = 0.5
 for _ in range(240):
